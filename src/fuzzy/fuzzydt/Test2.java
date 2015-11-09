9f3fb968bd1af06a2a47087727d7210bbe8f4884
MHJ <MHJ@10.30.103.88>
2015-11-09 12:50:06 +0100

    Add the paper example

--- src/fuzzy/fuzzydt/Test2.java
+++ src/fuzzy/fuzzydt/Test2.java
@@ -8,44 +8,55 @@ package fuzzy.fuzzydt;
 import fuzzy.data.Attribute;
 import fuzzy.data.Dataset;
 import fuzzy.data.Row;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
  *
  * @author Mohammed
  */
 public class Test2 {
-    public static void main(String[] args) {
-                //Create a dataset
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         Dataset d = new Dataset("Sample1");
         
-        // Add the attributes with Linguistic terms
-        d.addAttribute(new Attribute("Outlook", new String[] {"Sunny", "Cloudy", "Rain"}));
-        d.addAttribute(new Attribute("Temprature", new String[] {"Hot", "Mild", "Cool"}));
-        d.addAttribute(new Attribute("Humidity", new String[] {"Humid", "Normal"}));
-        d.addAttribute(new Attribute("Wind", new String[] {"Windy", "Not_Windy"}));
-        d.addAttribute(new Attribute("Plan", new String[] {"Volleyball", "Swimming", "W_lifting"}));
+        BufferedReader br = new BufferedReader(new FileReader("data/test.txt"));
         
+        String line = "";
+        String className = "";
+        while((line = br.readLine()) != null) {
+            if(line.startsWith("#")) {
+                String [] terms = line.substring(line.indexOf(":") + 1).split(" ");
+                d.addAttribute(new Attribute(line.substring(1, line.indexOf(":")), terms));
+            }
+            else if(line.startsWith("@")) {
+                String [] terms = line.substring(line.indexOf(":") + 1).split(" ");
+                d.addAttribute(new Attribute(line.substring(1, line.indexOf(":") ), terms));                
+                className = line.substring(1, line.indexOf(":"));
+            }
+            else if(line.startsWith("=ROW=")) {
+                String [] data = line.split(" ");
+                Object[] crispRow = new Object[d.getAttributesCount()];
+                double[][] fuzzyValues = new double[d.getAttributesCount()][];
+                int k = 1;
+                for(int i = 0; i < crispRow.length; i++) {
+                    crispRow[i] = "Dummy";
+                    fuzzyValues[i] = new double[d.getAttribute(i).getLinguisticTermsCount()];
+                    for(int j = 0; j < fuzzyValues[i].length; j++) {
+                        fuzzyValues[i][j] = Double.parseDouble(data[k++]);
+                    }
+                }
+                
+                d.addRow(new Row(crispRow, fuzzyValues));
+                
+                
+            }
+        }
         
-
+        br.close();
         
-        d.addRow(new Row(new Object[]{"Dummy", "Dummy", "Dummy", "Dummy", "Dummy"}, new double[][] {{ 0.9, 0.1, 0.0}, {1.0, 0.0, 0.0}, {0.8, 0.2}, {0.4, 0.6}, {0.0, 0.8, 0.2  }}));
-        d.addRow(new Row(new Object[]{"Dummy", "Dummy", "Dummy", "Dummy", "Dummy"}, new double[][] {{ 0.8, 0.2, 0.0}, {0.6, 0.4, 0.0}, {0.0, 1.0}, {0.0, 1.0}, {1.0, 0.7, 0.0  }}));
-        d.addRow(new Row(new Object[]{"Dummy", "Dummy", "Dummy", "Dummy", "Dummy"}, new double[][] {{ 0.0, 0.7, 0.3}, {0.8, 0.2, 0.0}, {0.1, 0.9}, {0.2, 0.8}, {0.3, 0.6, 0.1  }}));
-        d.addRow(new Row(new Object[]{"Dummy", "Dummy", "Dummy", "Dummy", "Dummy"}, new double[][] {{ 0.2, 0.7, 0.1}, {0.3, 0.7, 0.0}, {0.2, 0.8}, {0.3, 0.7}, {0.9, 0.1, 0.0  }}));
-        d.addRow(new Row(new Object[]{"Dummy", "Dummy", "Dummy", "Dummy", "Dummy"}, new double[][] {{ 0.0, 0.1, 0.9}, {0.7, 0.3, 0.0}, {0.5, 0.5}, {0.5, 0.5}, {0.0, 0.0, 1.0  }}));
-        d.addRow(new Row(new Object[]{"Dummy", "Dummy", "Dummy", "Dummy", "Dummy"}, new double[][] {{ 0.0, 0.7, 0.3}, {0.0, 0.3, 0.7}, {0.7, 0.3}, {0.4, 0.6}, {0.2, 0.0, 0.8  }}));
-        d.addRow(new Row(new Object[]{"Dummy", "Dummy", "Dummy", "Dummy", "Dummy"}, new double[][] {{ 0.0, 0.3, 0.7}, {0.0, 0.0, 1.0}, {0.0, 1.0}, {0.1, 0.9}, {0.0, 0.0, 1.0  }}));
-        d.addRow(new Row(new Object[]{"Dummy", "Dummy", "Dummy", "Dummy", "Dummy"}, new double[][] {{ 0.0, 1.0, 0.0}, {0.0, 0.2, 0.8}, {0.2, 0.8}, {0.0, 1.0}, {0.7, 0.0, 0.3  }}));
-        d.addRow(new Row(new Object[]{"Dummy", "Dummy", "Dummy", "Dummy", "Dummy"}, new double[][] {{ 1.0, 0.0, 0.0}, {1.0, 0.0, 0.0}, {0.6, 0.4}, {0.7, 0.3}, {0.2, 0.8, 0.0  }}));
-        d.addRow(new Row(new Object[]{"Dummy", "Dummy", "Dummy", "Dummy", "Dummy"}, new double[][] {{ 0.9, 0.1, 0.0}, {0.0, 0.3, 0.7}, {0.0, 1.0}, {0.9, 0.1}, {0.0, 0.3, 0.7  }}));
-        d.addRow(new Row(new Object[]{"Dummy", "Dummy", "Dummy", "Dummy", "Dummy"}, new double[][] {{ 0.7, 0.3, 0.0}, {1.0, 0.0, 0.0}, {1.0, 0.0}, {0.2, 0.8}, {0.4, 0.7, 0.0  }}));
-        d.addRow(new Row(new Object[]{"Dummy", "Dummy", "Dummy", "Dummy", "Dummy"}, new double[][] {{ 0.2, 0.6, 0.2}, {0.0, 1.0, 0.0}, {0.3, 0.7}, {0.3, 0.7}, {0.7, 0.2, 0.1  }}));
-        d.addRow(new Row(new Object[]{"Dummy", "Dummy", "Dummy", "Dummy", "Dummy"}, new double[][] {{ 0.9, 0.1, 0.0}, {0.2, 0.8, 0.0}, {0.1, 0.9}, {1.0, 0.0}, {0.0, 0.0, 1.0  }}));
-        d.addRow(new Row(new Object[]{"Dummy", "Dummy", "Dummy", "Dummy", "Dummy"}, new double[][] {{ 0.0, 0.9, 0.1}, {0.0, 0.9, 0.1}, {0.1, 0.9}, {0.7, 0.3}, {0.0, 0.0, 1.0  }}));
-        d.addRow(new Row(new Object[]{"Dummy", "Dummy", "Dummy", "Dummy", "Dummy"}, new double[][] {{ 0.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {1.0, 0.0}, {0.8, 0.2}, {0.0, 0.0, 1.0  }}));
-        d.addRow(new Row(new Object[]{"Dummy", "Dummy", "Dummy", "Dummy", "Dummy"}, new double[][] {{ 1.0, 0.0, 0.0}, {0.5, 0.5, 0.0}, {0.0, 1.0}, {0.0, 1.0}, {0.8, 0.6, 0.0  }}));
-        
-        GFID3 gfid3 = new GFID3(0.75);
+        GFID3 gfid3 = new GFID3(0.8);
         
         TreeNode root = gfid3.buildTree(d, "Plan");
         
